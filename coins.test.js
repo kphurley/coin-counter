f2bd7d40b4a5b1130ea58d4e34f1e95226bac58a
Kevin Hurley <kphurley@meraki.com>
2023-08-04 13:41:09 -0500

    Add second test case

--- coins.test.js
+++ coins.test.js
@@ -5,4 +5,8 @@ describe('Coins', () => {
   it('returns 4, for an array of [1,2,3] and a sum of 4', () => {
     assert.equal(coinChanger([1,2,3], 4), 4);
   });
+
+  it('returns 5, for an array of [2, 5, 3, 6] and a sum of 10', () => {
+    assert.equal(coinChanger([2, 5, 3, 6], 10), 5);
+  });
 });
